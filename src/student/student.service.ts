d6a9d7614c3d4161f471dcba151621d8deb55564
SArminkaS <arminka4320@outlook.hu>
2025-04-04 12:53:32 +0200

    create student service methods

--- src/student/student.service.ts
+++ src/student/student.service.ts
@@ -1,4 +1,23 @@
 import { Injectable } from '@nestjs/common';
+import { Student } from 'src/models/student.model';
 
 @Injectable()
-export class StudentService {}
+export class StudentService {
+    async getAll(currentPage:number, perPage:number)
+    {
+        return await Student.findAll({offset:(currentPage - 1)*perPage,limit:perPage})
+    }
+    async addOne(student: {id1:any,name:string,email:string})
+    {
+        return await Student.create(student)
+    }
+    async updateOne(id:any, fields: {name:string,email:string})
+    {
+        return await Student.update(fields,{where:{id1:id}})
+    }
+
+    async deleteOne(id:any)
+    {
+        return await Student.destroy({where:{id1:id}})
+    }
+}
