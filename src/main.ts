975e11632f72d7c741b708133fd145e39703b221
SArminkaS <arminka4320@outlook.hu>
2025-04-05 12:20:28 +0200

    add another layer of validation

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { HttpExceptionFilter } from './exception-filters/http-exception.filter';
 import { ValidationFilter } from './exception-filters/validation-exceptin.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalFilters(new HttpExceptionFilter())
-  app.useGlobalFilters(new ValidationFilter())
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalFilters(new ValidationFilter());
+  app.useGlobalPipes(new ValidationPipe({transform:true}));
+  await app.listen(process.env.PORT ?? 3000)
 }
 bootstrap();
