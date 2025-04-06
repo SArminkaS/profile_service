04d56c2f04760425dcff65f25066ed4b9601eb2b
SArminkaS <arminka4320@outlook.hu>
2025-04-06 13:49:41 +0200

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: process.env.CORS_ORIGIN,
+    methods:'GET,POST,PUT,DELETE'
+  })
   app.useGlobalFilters(new SeuqelizeValidationFilter());
   app.useGlobalPipes(new ValidationPipe({transform:true}));
   await app.listen(process.env.PORT ?? 3001, process.env.HOST || '127.0.0.1')
